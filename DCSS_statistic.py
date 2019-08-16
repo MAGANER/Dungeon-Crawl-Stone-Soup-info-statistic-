9ec9a08fa077550c44d55e4af3d3d89b73d611b8
MAGANER <maganer.krik@mail.ru>
2019-08-16 21:31:20 +0300

    now i can get data about turns and gold

--- DCSS_statistic.py
+++ DCSS_statistic.py
@@ -2,7 +2,8 @@ import os
 
 #get path, including hard-ware,(E:/DCSS for example)
 dcss_path = input("enter DCSS path:")
-all_files = os.listdir(dcss_path+"/morgue/")
+file_dir = dcss_path+"/morgue/"
+all_files = os.listdir(file_dir)
 
 #get only files with character data
 txt_only = []
@@ -11,3 +12,39 @@ for str in all_files:
 	if str[size-3:size+1] == "txt":
 		txt_only.append(str)
 		
+#read all data 
+data = []
+for file in txt_only:
+	current_file = open(file_dir+file)
+	data_str = current_file.read()
+	data.append(data_str)
+	current_file.close()
+
+#returns list with values of passed data
+def get_data_str(data_to_get):
+	values = []
+	for str in data:
+	    #get value between : and ,
+		position = str.find(data_to_get)
+		size = len(data_to_get)
+		
+		start = position+size+1 #pos after :
+		end = str.find(",",start, len(str)) # pos before ,
+			
+		value = str[start:end]
+		values.append(value)
+		
+	return values
+
+def clear_list(your_list, start,end):
+	new_list = []
+	for str in your_list:
+		str = str[start:end+1]
+		new_list.append(str)
+	your_list.clear()
+	return new_list
+		
+turns = get_data_str("Turns")
+gold = get_data_str("collected") # collected x gold
+gold = clear_list(gold,0,1)
+
