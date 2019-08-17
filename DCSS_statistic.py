911654606cbefba56e499c2b175e48fd710f758c
MAGANER <maganer.krik@mail.ru>
2019-08-17 18:27:17 +0300

    added ability to compute whole game time

--- DCSS_statistic.py
+++ DCSS_statistic.py
@@ -1,5 +1,6 @@
 import os
 
+
 #get path, including hard-ware,(E:/DCSS for example)
 dcss_path = input("enter DCSS path:")
 file_dir = dcss_path+"/morgue/"
@@ -35,6 +36,19 @@ def get_data_str(data_to_get, before_symb, after_symb_pos):
 			value = int(str[start:end])
 			values.append(value)		
 	return values
+def get_raw_data_str(data_to_get, before_symb, after_symb_pos):
+	values = []
+	for str in data:
+		#get value between : and ,
+		position = str.find(data_to_get)
+		size = len(data_to_get)
+		
+		start = position+size+after_symb_pos #pos after :
+		end = str.find(before_symb,start, len(str)) # pos before symbol
+		
+		value = str[start:end]
+		values.append(value)		
+	return values	
 	
 #True -> number, False -> string
 def is_number(str):
@@ -52,9 +66,82 @@ def combine_list_elems(your_list):
 	for elem in your_list:
 		summ= summ + elem
 	return summ
+
+#game could be very short, so it wasn't wrote in morgue	
+def clear_shit_outta_time_list(time):
+	new_time = []
+	for elem in time:
+		if is_number(elem[0]):
+			new_time.append(elem)
+	
+	return new_time
+
+
+def is_null(number):
+	if int(number) == 0:
+		return True
+	else:
+		return False
+def compute_whole_game_time(time):
+	hours = 0
+	minutes = 0
+	seconds = 0
+	for elem in time:
+		#get data
+		hour = int(elem[0:2])
+		minute = int(elem[3:5])
+		second = int(elem[6:9])
+		
+		#compute all
+		hours = hours + hour
+		minutes = minutes + minute
+		seconds = seconds + second
+	
+	final_seconds = final_minutes = final_hours = 0
+	
+	seconds_str = repr(seconds/60)
+	minutes_str = repr(minutes/60)
+	hours_str   = repr(hours/60)
+	
+	#compute seconds
+	_seconds = int(seconds_str[3:5])+ int(minutes_str[3:5])
+	if _seconds > 60:
+		_seconds = _seconds/60
+		
+		point_pos = repr(_seconds).find(".")
+		_minutes = int(repr(_seconds)[0:point_pos])
+		minutes = minutes + _minutes
+		minutes_str = repr(minutes/60) # update var
+		
+		_seconds = int(repr(_seconds)[point_pos+1:point_pos+3])
+
+	#compute minutes
+	_hours = 0
+	_minutes = float(minutes_str)
+	if _minutes > 60:
+		_minutes = _minutes/60
+	else:
+		point_pos = repr(_minutes).find(".")
+		_hours = int(repr(_minutes)[0:point_pos])
+		_minutes = int(repr(_minutes)[point_pos+1:point_pos+3])
+	
+	#compute hours
+	if hours > 0:
+		#my game never lasted more than 59 minutes
+		print("WOW, finish this script!")
+		
+	final_hours   = _hours
+	final_minutes = _minutes
+	final_seconds = _seconds
+	
+	return (final_hours,final_minutes,final_seconds)
+	
 	
 turns = get_data_str("Turns",",",1)
 gold = get_data_str("collected"," ",1) # collected x gold
+time = get_raw_data_str("lasted","(",1) # game lasted x time
+time = clear_shit_outta_time_list(time)
+game_time = compute_whole_game_time(time)
 
 whole_turns = combine_list_elems(turns)
 max_turns = max(turns)
@@ -72,3 +159,4 @@ print("whole gold you earn",whole_gold)
 print("max gold you earn:",max_gold)
 print("min gold you earn:",min_gold)
 
+print("you played:(h,m,s)",game_time)
